749299a2f681293a55764ad5a9d6c68f8b856b52
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-12-08 10:09:44 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import {gql, ApolloServer} from 'apollo-server'
+import axios from 'axios';
+
+const typeDefis= gql ` 
+     type personempleados {
+          id_empleado: Int
+          ap_paterno: String
+          ap_materno: String
+          nombre: String
+          fechaIngreso: String
+          antiguedad: String
+          dependencia: String
+          areaPuesto: String
+     }
+
+     type Query{
+         
+          allEmpleados: [personempleados]
+     }
+
+`;
+
+const resolvers = {
+     Query:{
+          
+          allEmpleados:async (root, args)=> {
+               const {data: personempleados} = await axios.get('http://localhost:3000/empleados')
+               console.log(personempleados)
+               return personempleados
+          },
+            
+     }
+}
+
+const server = new ApolloServer({
+     typeDefs: typeDefis,
+     resolvers
+})
+
+server.listen().then(({url}) => {
+     console.log( `server ${url} `)
+})
